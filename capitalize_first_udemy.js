f4910f7fd771b15a71aa85c9f89fb4b8a53e3076
Crew Spence <csspence123@gmail.com>
2020-07-15 05:39:14 -0500

    Created function stub for toy problem.

---
+++ capitalize_first_udemy.js
@@ -0,0 +1,12 @@
+/*
+JavaScript Algorithms and Data Structures Masterclass
+
+capitalizeFirst
+Write a recursive function called capitalizeFirst.  Given an array of strings, captialize the first letter of each string in the array.
+*/
+
+function capitalizeFirst (arr, index) {
+
+}
+
+// capitalizeFirst(['car','taco','banana']); // ['Car','Taco','Banana']
